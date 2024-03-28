5bad0ebf3a5ab0864aa6c5d2ceff177dd4f00250
Viktor Nilsson <viktornilsson91@live.se>
2024-03-28 09:54:41 +0100

    Add project

---
+++ DotNetLambdaFunction.Tests/FunctionTest.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Lambda.TestUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace DotNetLambdaFunction.Tests
+{
+    [TestClass]
+    public class FunctionTest
+    {
+        [TestMethod]
+        public async Task Run_FunctionHandler()
+        {
+            var function = new Function();
+            var context = new TestLambdaContext();
+
+            await function.FunctionHandler(context);
+
+            Assert.IsNotNull(context);
+        }
+    }
+}
---
+++ DotNetLambdaFunction/Function.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+
+namespace DotNetLambdaFunction
+{
+    public class Function
+    {
+        public async Task FunctionHandler(ILambdaContext context)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
